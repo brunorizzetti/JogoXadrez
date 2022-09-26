b64620257a4e944ab902430de20d0a7897873bf4
brunorizzetti <bruno.rizzetti@gmail.com>
2022-09-26 15:17:57 -0300

    Imprimindo tabuleiro

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace JogoXadrez
     {
         private static void Main(string[] args)
         {
-            Posicao p = new Posicao(3,4);
+            Tabuleiro tab = new Tabuleiro(8,8);
 
-            System.Console.WriteLine("Posição: " + p);
+            Tela.imprimirTabuleiro(tab);
         }
     }
 }
